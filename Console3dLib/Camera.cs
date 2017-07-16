6f709b680d1a87878cd73816fa27b9cd21aea8aa
Peter H <mlhusman@earthlink.net>
2017-07-16 16:31:02 -0700

    Created project, learned about integral/derivative

---
+++ Console3dLib/Camera.cs
@@ -0,0 +1,23 @@
+﻿using Console3dLib.CoreTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console3dLib
+{
+    class Camera
+    {
+        public Vector3d Position
+        {
+            get;
+            set;
+        }
+        public Vector3d Rotation
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Console3dLib/CoreTypes/3dVector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console3dLib.CoreTypes
+{
+    public class Vector3d
+    {
+        public float X;
+        public float Y;
+        public float Z;
+        public Vector3d (float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        /// <summary>
+        /// Z: +1
+        /// </summary>
+        public Vector3d Up { get { return new Vector3d(0f, 0f, 1f); } }
+
+        /// <summary>
+        /// X: +1
+        /// </summary>
+        public Vector3d Forward { get { return new Vector3d(1f, 0f, 0f); } }
+
+        /// <summary>
+        /// Y: +1
+        /// </summary>
+        public Vector3d Side { get { return new Vector3d(0f, 1f, 0f); } }
+    }
+}
